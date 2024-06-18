774f6e20bd6c72407a0bca12bb6c0df66440ba42
kapsarovL <kapsarovlazar@gmail.com>
2024-06-18 21:22:45 +0200

    first commit

---
+++ serviceworker.js
@@ -0,0 +1,25 @@
+// V 2
+const assets = ["/", "styles.css", "app.js", "sw-register.js",
+    "https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2"];
+
+    self.addEventListener("install", event => {
+        event.witUntil(
+            caches.open("assets").then(cache => {
+                cache.addAll(assets);
+            })
+        );
+    });
+
+    // State while revalidate strategy
+    self.addEventListener("fetch", event => {
+        event.respondWith(
+            caches.match(event.request).then(cacheResponse => {
+                return cacheResponse || fetch(event.request).then(fetchResponse => {
+                    return caches.open("assets").then(cache => {
+                        cache.put(event.request, fetchResponse.clone());
+                        return fetchResponse;
+                    });
+                });
+            })
+        );
+    });
